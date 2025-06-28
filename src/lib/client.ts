66d91bbbef2708d622a29759b721d2933167c2b3
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-06-28 03:39:21 +0100

    fix: damn, who gave me a computer, fixed jstack baseurl

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import type { AppRouter } from "@/server"
 import { createClient } from "jstack"
 
 export const client = createClient<AppRouter>({
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: `${process.env.BETTER_AUTH_URL}/api`,
 })

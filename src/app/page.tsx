7e9502a67e767e2130b402f3f0f03a5a54b5bd04
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-06-28 02:59:50 +0100

    fix: removed unused props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home() {
       </nav>
 
       <main className="py-6">
-       <TodosContainer signedInUserId={session.user.id} />
+       <TodosContainer />
       </main>
     </div>
   )

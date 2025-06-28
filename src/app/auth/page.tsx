64ae5a287e520ad8ec7858c3b069133fc659b25b
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-06-28 03:23:18 +0100

    fix: fixed overflow issues on smaller screens

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,8 +8,8 @@ export default async function Page() {
    if(session) redirect("/")
    
    return (
-      <main className="w-screen h-screen px-4 sm:px-6 grid place-items-center bg-secondary dark:bg-background">
-         <div className="w-[380px] p-6 text-center bg-background border dark:border-border/30 dark:bg-secondary rounded-lg">
+      <main className="w-screen h-[100dvh] px-4 sm:px-6 grid place-items-center bg-secondary dark:bg-background">
+         <div className="max-w-[380px] w-full p-6 text-center bg-background border dark:border-border/30 dark:bg-secondary rounded-lg">
             <h2 className="text-2xl font-medium text-secondary-foreground">Welcome Back</h2>
             <p className="text-muted-foreground text-sm">Sign in to continue with your todos</p>
             <SigninButton />

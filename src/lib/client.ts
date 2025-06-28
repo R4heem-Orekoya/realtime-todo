5f088a69343d38df3468484144eec40c62c01eef
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-06-28 03:49:13 +0100

    fix: fixed env naming issues

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import type { AppRouter } from "@/server"
 import { createClient } from "jstack"
 
 export const client = createClient<AppRouter>({
-  baseUrl: `${process.env.BETTER_AUTH_URL}/api`,
+  baseUrl: `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api`,
 })
